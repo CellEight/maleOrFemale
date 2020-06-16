4d3681b9293ee83237629633b068c1c0a8692776
Lewis Patten <lp921@york.ac.uk>
2020-06-16 23:04:18 +0100

    Update genderRecognition.py

--- genderRecognition.py
+++ genderRecognition.py
@@ -3,6 +3,7 @@ from fastai.metrics import error_rate
 import cv2
 import threading
 
+
 path = Path('./')
 learn = load_learner(path)
 
@@ -11,10 +12,13 @@ camera = cv2.VideoCapture(0)
 cv2.namedWindow("test")
 
 while True:
-    _, img = camera.read()
+    ret_val, img = camera.read()
     cv2.imshow("test", img)
-    img = Image(torch.from_numpy(img))
-    _,_,outputs = learn.predict(img)
+    image = Image(pil2tensor(img, dtype=np.float32).div_(255))
+    #print(image)
+    _,_,outputs = learn.predict(image)
+    output.normalise()
     gender = ("Male" if outputs[0]>= outputs[1] else "Female")
-    print(f"Looks like they're {gender} - P(Male)={round(outputs[0],3)}, P(Female)={round(outputs[1],3)}")
+    print(f"Looks like they're {gender} - P(Male)={outputs[0]}, P(Female)={outputs[1]}")
+    cv2.waitKey(33)
 camera.release()
